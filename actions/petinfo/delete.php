ca2a7c44d526c89aaa46c342c3781b3ee44c288e
Alice Hutley <alice.hutley@gmail.com>
2020-01-29 11:12:26 +1100

    Modifying table view

---
+++ actions/petinfo/delete.php
@@ -0,0 +1,23 @@
+<?php
+
+function delete_ALL(Web $w) {
+
+    // start by finding the item id included in the URL
+    $p = $w->pathMatch('id');
+    // check to see if the id has been found
+    if (empty($p['id'])) {
+        // if no id found use the 'error' function to redirect the use to a safe page and display a message.
+        $w->error('No id found for item','example');
+    }
+    // use the id to retrieve the item
+    $item = $w->creaturetopia->getPetInfoForId($p['id']);
+    // check to see if the item was found
+    if (empty($item)) {
+        // no item found so let the user know
+        $w->error('No item found for id','example');
+    }
+    // delete the item
+    $item->delete();
+    // redirect the user back to the item list with a message
+    $w->msg('Item deleted','example');
+}
