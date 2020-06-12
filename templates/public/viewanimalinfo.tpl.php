b5f7c308f78f8600ef1f567a26a8414861caf305
Alice <alice.hutley@gmail.com>
2020-06-12 21:51:40 +1000

    adding a target of new tab/window to links

--- templates/public/viewanimalinfo.tpl.php
+++ templates/public/viewanimalinfo.tpl.php
@@ -70,7 +70,7 @@
             For information about differnt breeds in this species, click the link below:
           </p>
           <nav>
-            <p><a href="#">Breeds</a></p>
+            <p><a href="#" target="-blank">Breeds</a></p>
           </nav>
         </div>
       </section>
