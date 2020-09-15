acea1701c7edbfde40e2df5207b8f217410a4a1e
Alice <alice.hutley@gmail.com>
2020-09-15 12:17:54 +1000

    for cmfive-core insight module

---
+++ models/InsightTestClass2.php
@@ -0,0 +1,15 @@
+<?php
+
+class InsightTestClass2 extends InsightBaseClass
+{
+    public $name="Test Insight 2";
+    public $description="Test model for insights in creaturetopia respository";
+
+    public function getFilters()
+    {
+    }
+
+    public function run()
+    {
+    }
+}
