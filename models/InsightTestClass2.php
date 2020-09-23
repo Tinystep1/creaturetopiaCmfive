3dd818e54876afdbf654bf270af66bc48f979715
Alice <alice.hutley@gmail.com>
2020-09-23 10:38:41 +1000

    Updating to fit with InsightBaseClass specifications

--- models/InsightTestClass2.php
+++ models/InsightTestClass2.php
@@ -5,11 +5,11 @@ class InsightTestClass2 extends InsightBaseClass
     public $name="Test Insight 2";
     public $description="Test model for insights in creaturetopia respository";
 
-    public function getFilters()
+    public function getFilters(Web $w): array
     {
     }
 
-    public function run()
+    public function run(Web $w, array $params = []): array
     {
     }
 }
